c312740c1fa1a8995a7327360b244b12ff8c7938
beomsu6567 <beomsu6567@gmail.com>
2023-11-04 17:41:46 +0900

    最新

--- data/themes/THEME-NAME/js/common.js
+++ data/themes/THEME-NAME/js/common.js
@@ -1,10 +1,12 @@
-var PC_FIXED = false;
-var SP_FIXED = false;
-var SP_WIDTH = 769;
-var SPEED = 500;
+const PC_FIXED = false;
+const SP_FIXED = false;
+const SP_WIDTH = 769;
+const SPEED = 500;
+
+// 1. スクロール関連
 
 function scrollPosition(position) {
-  var offsetFromTop = $('header')
+  let offsetFromTop = $('header');
   position -= PC_FIXED && $(window).innerWidth() >= SP_WIDTH || SP_FIXED && $(window).innerWidth() < SP_WIDTH ? offsetFromTop.innerHeight() : 0;
   $('html, body').animate({
     scrollTop: position
@@ -12,50 +14,97 @@ function scrollPosition(position) {
 }
 
 $(function () {
-  var menu_btn = $('.slidemenu-btn');
-  var body = $(document.body);
-  var menu_open = false;
+  let body = $(document.body);
+  let menu_open = false;
+  let menu_btn = $('.slidemenu-btn');
+  // let menu_img = $(".slidemenu-btn").find('img'); // 画像のスライドメニューボタン
+  // let menu_txt = $(".slidemenu-btn").find('.ttl'); // テキストのスライドメニューボタン
 
-  //（<a href="#top">の様に記述すると滑らかにスクロールする。）
-  $('a[href*="#"]:not(.tab)').on('click', function (e) {
-    var current = $(location).attr('pathname')
-    var link = $(this).attr('href').split('#')[0];
-    if (current === link || link == "") {
-      e.preventDefault();
-      menu_btn.removeClass('active');
-      body.removeClass('open');
-      body.removeAttr('style');
-      menu_open = false;
-      var position = $(this.hash).length > 0 ? $(this.hash).offset().top : 0;
-      scrollPosition(position);
+  function slidemenuOpen() {
+    // $(menu_img).attr("src", $(menu_img).attr("src").replace("menu", "close"));
+    // $(menu_txt).text("close");
+    menu_btn.addClass('active');
+    body.addClass('open');
+    body.removeAttr('style');
+    menu_open = true;
+  }
+
+  function slidemenuClose() {
+    // $(menu_img).attr("src", $(menu_img).attr("src").replace("close", "menu"));
+    // $(menu_txt).text("menu");
+    menu_btn.removeClass('active');
+    body.removeClass('open');
+    body.removeAttr('style');
+    if (top != 0) {
+      // ページ内スクロールのため
+      $(window).scrollTop(top);
+      top = 0; //リセット
+    }
+    menu_open = false;
+  }
+
+  // <a href="#top">の様に記述すると滑らかにスクロールする。
+  $('a[href*="#"]').on('click', function (e) {
+    let current = location.pathname;
+    let full_current = location.origin + current;
+    let link = $(this).attr('href').split('#')[0];
+    // トップページ及びカテゴリトップの場合は、すべてindex.htmlありとして判定する
+    // aタグは@link()#00 or #00で指定すること（aaa.html#00は動きません）
+    if (link != '') {
+      if (link.indexOf('.html') == -1) {
+        if (link.slice(-1) == '/') {
+          link += 'index.html';
+        } else {
+          link += '/index.html';
+        }
+      }
+      if (full_current.indexOf('.html') == -1) {
+        if (full_current.slice(-1) == '/') {
+          full_current += 'index.html';
+        } else {
+          full_current += '/index.html';
+        }
+      }
+    }
+
+    if (current === link || full_current === link || link == "") {
+      if ($(this).attr('data-href')) {
+        // tabjsとの競合回避
+        return false;
+      } else {
+        e.preventDefault();
+        slidemenuClose();
+        let position = $(this.hash).length > 0 ? $(this.hash).offset().top : 0;
+        scrollPosition(position);
+      }
     }
   });
 
-  //スライドメニューの開閉
-  var top = 0;
+  // スライドメニューの開閉
+  let top = 0;
   menu_btn.on('click', function () {
     if (!menu_open) {
       top = $(window).scrollTop();
     }
-    body.toggleClass('open');
-    menu_btn.toggleClass('active');
-    menu_open = true;
     if (body.hasClass('open')) {
+      slidemenuClose();
+    } else {
+      slidemenuOpen();
       body.css({
         'height': window.innerHeight,
         'top': -top
       });
-    } else {
-      body.removeAttr('style');
-      $(window).scrollTop(top);
-      menu_open = false;
     }
   });
+  // PC時overlayを設置する場合
+  // $("#overlay").on("click", function () {
+  //   slidemenuClose();
+  // });
 });
 
 // 一定量スクロールするとページトップに戻るが表示される（場所等の指定はcommon.scssにて）
 $(function () {
-  var top_btn = $('.pagetop');
+  let top_btn = $('.pagetop');
   top_btn.hide();
   $(window).scroll(function () {
     $(this).scrollTop() > 100 ? top_btn.fadeIn() : top_btn.fadeOut();
@@ -65,28 +114,40 @@ $(function () {
     scrollPosition(0);
     return false;
   });
-});
 
-//アンカーリンク付きのページ遷移をするとき：ヘッダーが固定分調整するjs
-$(function () {
-  $(document).on('ready', function () {
-    if (location.hash != "") {
-      var pos = $(location.hash).offset().top;
-      if (PC_FIXED && $(window).innerWidth() >= SP_WIDTH || SP_FIXED && $(window).innerWidth() < SP_WIDTH) {
-        pos -= $('header').innerHeight();
-        $("html, body").animate({
-          scrollTop: pos
-        }, 1, "swing");
-      } else {
-        return false;
+  // 1.5秒で表示消す
+  if ($(window).innerWidth() < SP_WIDTH) {
+    var scrollStopEvent = new $.Event("scrollstop");
+    var delay = 1500;
+    var timer;
+    function scrollStopEventTrigger() {
+      if (timer) {
+        clearTimeout(timer);
       }
+      timer = setTimeout(function () {
+        top_btn.fadeOut();
+        $(window).trigger(scrollStopEvent)
+      }, delay);
     }
-  });
+    $(window).on("scroll", scrollStopEventTrigger);
+  };
 });
 
-// ※※※※※　スクロール関連ここまで　※※※※※
+// アンカーリンク付きのページ遷移をするとき：ヘッダーが固定分調整するjs
+$(window).on('load', function () {
+  if (location.hash != "" && $(location.hash).length > 0) {
+    if (PC_FIXED && $(window).innerWidth() >= SP_WIDTH || SP_FIXED && $(window).innerWidth() < SP_WIDTH) {
+      let pos = $(location.hash).offset().top - $('header').innerHeight();
+      $("html, body").animate({
+        scrollTop: pos
+      }, 1, "swing");
+    } else {
+      return false;
+    }
+  }
+});
 
-// rollover（_offと末尾についた画像をオンマウスで_onとついた画像に切り替える）
+// 2. rollover（_offと末尾についた画像をオンマウスで_onとついた画像に切り替える）
 $(function () {
   $('img').hover(
     function () {
@@ -98,14 +159,13 @@ $(function () {
   );
 });
 
-
-//横幅375px以下のviewportの設定
+// 3. 横幅375px以下のviewportの設定
 new ViewportExtra(375)
 
-//httpが含まれる場合にwordbreakを付与するjs
-//直下のテキストのみを取得するプラグイン
+// 4. httpが含まれる場合にwordbreakを付与するjs
+// 直下のテキストのみを取得するプラグイン
 $.fn.textNodeText = function () {
-  var result = "";
+  let result = "";
   $(this).contents().each(function () {
     if (this.nodeType === 3 && this.data) {
       result += jQuery.trim($(this).text());
@@ -116,15 +176,20 @@ $.fn.textNodeText = function () {
 //httpが含まれる場合にwordbreakを付与
 $(function () {
   $("*").each(function () {
-    var http = $(this).textNodeText();
+    let http = $(this).textNodeText();
     if (http.match(/http/)) {
       $(this).css("word-break", "break-all");
     }
   });
 });
 
-//youtubeの自動レスポンシブ対応
-//iframeタグの親要素に、divタグが追加される
+// 5. youtubeの自動レスポンシブ対応
+// iframeタグの親要素に、divタグが追加される
 $(function () {
   $('iframe[src*="youtube"]').wrap('<div class="youtube-wrap"></div>');
 });
+
+// ------------------------------------------------------------
+// 追加分ここから
+// スライドメニューに関しては、slidemenuOpen/slidemenuCloseへ記載する
+// ------------------------------------------------------------
---
+++ data/themes/THEME-NAME/js/module/form/jquery.resultStay.js
@@ -0,0 +1,202 @@
+/*
+  UPDATE 2022.10.20
+  ver 1.3
+  1.0 2022.5.13 CREATED
+  1.1 2022.10.12 UPDATED checkboxParent追加
+  1.2 2022.10.19 UPDATED モーダルへの対応: checkboxOutput追加, text追加
+  1.3 2022.10.20 UPDATED タグ複数軸へ対応
+*/
+(function ($) {
+  $.fn.resultStay = function (params) {
+    var defs = {
+      type: ['radio', 'checkbox', 'text', 'select'], //ボタン(['radio','checkbox','text','selectbox'])
+      checkboxValue: 'value', //value,name,id
+      checkboxParent: [], //checkboxの親要素
+      checkboxOutput: [], //checkした項目を一括表示する欄※parentと並べること
+      checkboxTag: [], //タグ使用時の複数軸対応 ※全てcheckboxであること
+      autoSubmit: false
+    };
+    // データ上書き
+    var config = $.extend({}, defs, params);
+    var form = this;
+    var type_setting = config.type;
+    var checkbox_setting = {
+      value: config.checkboxValue,
+      parent: config.checkboxParent,
+      output: config.checkboxOutput,
+      text: [],
+      tag: config.checkboxTag,
+      submit: config.autoSubmit,
+    };
+
+    //パラメータ取得
+    let arg = new Object;
+    let jp_search = decodeURI(location.search); //urlを日本語のまま取得
+    let pair = jp_search.substring(1).split('&'); //パラメータをそれぞれ取得
+    for (let i = 0; pair[i]; i++) {
+      let kv = pair[i].split('=');
+      // ASCII文字変換（必要であれば追加する）
+      kv[1] = kv[1].replace(/\+/g, ' ');
+      kv[1] = kv[1].replace(/\%2C/g, ',');
+      arg[kv[0]] = kv[1];
+    }
+
+    // radio, text, select(ここから)
+    function loadingCheck(type, selecter) {
+      selecter.each(function () {
+        let target_name = $(this).prop('name');
+        s_name = '[name="' + target_name + '"]';
+        //nameが照合できた場合（検索対象の場合）
+        if (arg[target_name]) {
+          switch (type) {
+            case 'radio':
+              if (arg[target_name] == $(this).val()) {
+                $(this).prop('checked', true);
+              }
+              break;
+            case 'text':
+            case 'select':
+              $(s_name).val(arg[target_name]);
+              break;
+          }
+        }
+      })
+    }
+
+    for (let i = 0; type_setting.length > i; i++) {
+      switch (type_setting[i]) {
+        case 'radio':
+        case 'text':
+          target_selector = form.find('input[type="' + type_setting[i] + '"]');
+          loadingCheck(type_setting[i], target_selector);
+          break;
+        case 'select':
+          target_selector = form.find(type_setting[i]);
+          loadingCheck(type_setting[i], target_selector);
+          break;
+        case 'checkbox':
+          // 読み込み時処理に含める
+          break;
+      }
+    }
+    // radio, text, select(ここまで)
+
+    // checkbox（ここから）
+    if (type_setting.includes('checkbox')) {
+      // checkbox用function
+      function checkStep(arg, input_name, loading, i, j) {
+        let textVal = '';
+        let double = false;
+        for (k = 0; arg.length > k; k++) {
+          if ($($(checkbox_setting.parent[i])[j]).find('input[type="checkbox"][' + checkbox_setting.value + '="' + arg[k] + '"]').length > 0) {
+            if (loading) {
+              // checkbox_value,checked
+              $($(checkbox_setting.parent[i])[j]).find('input[type="checkbox"][' + checkbox_setting.value + '="' + arg[k] + '"]').prop('checked', true);
+              checkbox_value[input_name].push(arg[k]);
+            }
+            if (double) {
+              textVal += ',';
+            } else {
+              double = true;
+            }
+            if (checkbox_setting.tag[i]) {
+              if ($($(checkbox_setting.parent[i])[j]).find('input[type="checkbox"][' + checkbox_setting.value + '="' + arg[k] + '"]').attr('data-name')) {
+                textVal += $($(checkbox_setting.parent[i])[j]).find('input[type="checkbox"][' + checkbox_setting.value + '="' + arg[k] + '"]').attr('data-name');
+              } else {
+                textVal += $($(checkbox_setting.parent[i])[j]).find('input[type="checkbox"][' + checkbox_setting.value + '="' + arg[k] + '"]').val();
+              }
+            } else {
+              textVal += arg[k];
+            }
+          }
+        }
+        if (checkbox_setting.output[i]) {
+          if (loading) {
+            checkbox_setting.text[i] = $(checkbox_setting.output[i]).text();
+          }
+          if (textVal) {
+            $(checkbox_setting.output[i]).text(textVal);
+          } else {
+            $(checkbox_setting.output[i]).text(checkbox_setting.text[i]);
+          }
+        } else {
+          checkbox_setting.text[i] = '';
+        }
+        if (checkbox_setting.submit) {
+          $(form).submit();
+        }
+      }
+
+      var checkbox_value = [];
+      for (let i = 0; checkbox_setting.parent.length > i; i++) {
+        let input_name = "";
+        // parentごとのループ
+        if ($(checkbox_setting.parent[i]).length > 0) {
+
+          // 同じparentが複数存在する場合のループ
+          for (let j = 0; $(checkbox_setting.parent[i]).length > j; j++) {
+            // 読み込み時
+            if (checkbox_setting.tag[i]) {
+              input_name = 'tags';
+            } else if ($($(checkbox_setting.parent[i])[j]).attr('data-search')) {
+              input_name = $($(checkbox_setting.parent[i])[j]).attr('data-search');
+            } else {
+              input_name = checkbox_setting.parent[i].slice(1);
+            }
+            if (!checkbox_value[input_name]) {
+              checkbox_value[input_name] = [];
+            }
+            if ($('input[name="' + input_name + '"]').length == 0) {
+              form.append('<input id="" name="' + input_name + '" type="hidden" value="" checked>')
+            }
+            if (arg[input_name]) {
+              // val設置
+              $('input[name = "' + input_name + '"]').val(arg[input_name]);
+
+              // checkbox_value,checked,text
+              checkStep(arg[input_name].split(','), input_name, true, i, j);
+            }
+
+            // クリック時
+            $($(checkbox_setting.parent[i])[j]).find('input[type="checkbox"]').change(function (e) {
+              // checkbox_value,checked
+              if (checkbox_setting.tag[i]) {
+                input_name = 'tags';
+              } else if ($($(checkbox_setting.parent[i])[j]).attr('data-search')) {
+                input_name = $($(checkbox_setting.parent[i])[j]).attr('data-search');
+              } else {
+                input_name = checkbox_setting.parent[i].slice(1);
+              }
+              if (e.target.checked) {
+                checkbox_value[input_name].push($(e.target).attr(checkbox_setting.value));
+              } else {
+                delete checkbox_value[input_name][$.inArray($(e.target).attr(checkbox_setting.value), checkbox_value[input_name])];
+              }
+
+              // val設置
+              let double = false;
+              let newVal = '';
+              $('input[name = "' + input_name + '"]').val('');
+              for (let num02 = 0; checkbox_value[input_name].length > num02; num02++) {
+                if (checkbox_value[input_name][num02]) { //emptyを除く
+                  if (double) {
+                    newVal += ',';
+                  } else {
+                    double = true;
+                  }
+                  newVal += checkbox_value[input_name][num02];
+                }
+              }
+              $('input[name = "' + input_name + '"]').val(newVal);
+
+              // text
+              checkStep(checkbox_value[input_name], input_name, false, i, j);
+            });
+          }
+        }
+      }
+    }
+    // checkbox（ここまで）
+
+  };
+})(jQuery);
